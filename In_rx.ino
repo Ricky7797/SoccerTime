53fa2f3f859bd42d0801ea4dd32b74406fee626d
Ricky7797 <47427820+Ricky7797@users.noreply.github.com>
2019-05-17 01:19:10 -0400

    Add files via upload

---
+++ In_rx.ino
@@ -0,0 +1,61 @@
+
+#include <RH_ASK.h>
+#include <SPI.h> // Not actualy used but needed to compile
+
+RH_ASK driver;
+
+struct dataStruct{
+  float press_norm ; 
+
+  unsigned long counter;
+   
+}myData;
+
+
+void setup()
+{
+    Serial.begin(9600);  // Debugging only
+    if (!driver.init())
+         Serial.println("init failed");
+}
+
+void loop()
+{
+    uint8_t buf[RH_ASK_MAX_MESSAGE_LEN];
+    uint8_t buflen = sizeof(buf);
+
+    if (driver.recv(buf, &buflen)) // Non-blocking
+    {
+  int i;
+
+  // Message with a good checksum received, dump it.
+ 
+        memcpy(&myData, buf, sizeof(myData));
+        Serial.println("");
+        
+                Serial.print("press_norm: ");
+        Serial.println(myData.press_norm);
+        if (myData.press_norm!=11.00) {
+          int x=random(3)+12;
+          Serial.println(x);
+          if (x!=14) {
+            Serial.println(x);
+            digitalWrite(x,HIGH);
+            delay(500);
+            digitalWrite(x,LOW);  
+          }
+          else {
+            Serial.println(99);
+            digitalWrite(13,HIGH);
+            digitalWrite(12,HIGH);
+            delay(500);
+            digitalWrite(13,LOW);
+            digitalWrite(12,LOW);
+          }
+          
+        }
+
+    }
+}
+
+
---
+++ In_tx.ino
@@ -0,0 +1,75 @@
+// ask_transmitter.pde
+// -*- mode: C++ -*-
+// Simple example of how to use RadioHead to transmit messages
+// with a simple ASK transmitter in a very simple way.
+// Implements a simplex (one-way) transmitter with an TX-C1 module
+
+#include <RH_ASK.h>
+#include <SPI.h> // Not actually used but needed to compile
+
+RH_ASK driver;
+
+
+struct dataStruct{
+  float press_norm ; 
+  unsigned long counter;
+   
+}myData;
+
+byte tx_buf[sizeof(myData)] = {0};
+
+int trigPin = 10;    // Trigger
+int echoPin = 11;    // Echo
+long duration, cm, inches;
+
+void setup()
+{
+    Serial.begin(9600);    // Debugging only
+    if (!driver.init())
+         Serial.println("init failed");
+         
+     
+  pinMode(trigPin, OUTPUT);
+  pinMode(4, OUTPUT);
+  pinMode(echoPin, INPUT); 
+}
+
+void loop()
+{
+ 
+  digitalWrite(trigPin, LOW);
+  delayMicroseconds(5);
+  digitalWrite(trigPin, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(trigPin, LOW);
+  pinMode(echoPin, INPUT);
+  duration = pulseIn(echoPin, HIGH);
+ 
+  cm = (duration/2) / 29.1;    
+  inches = (duration/2) / 74;
+  Serial.print(inches);
+  Serial.print("in, ");
+  Serial.print(cm);
+  Serial.print("cm");
+  Serial.println();
+  myData.press_norm=inches;    
+  delay(250);
+  if (myData.press_norm<24 && myData.press_norm>2) {
+    digitalWrite(4,HIGH);
+    Serial.println("Go");
+    delay(1000);
+    digitalWrite(4,LOW);
+  
+  memcpy(tx_buf, &myData, sizeof(myData) );
+  byte zize=sizeof(myData);
+
+driver.send((uint8_t *)tx_buf, zize);
+
+   // driver.send((uint8_t *)msg, strlen(msg));
+    driver.waitPacketSent();
+    myData.counter++;
+    delay(2000);
+  }
+}
+
+
